6a734c8c3b235e734f19da7b3158cc306c11ceb5
jesuisuncaillou <igorlacroute@gmail.com>
2015-08-27 13:11:54 +0200

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BdParallele::Application.config.secret_key_base = 'daf3d1c138331112c6cce2b955dbd21bd74a80e5d1d58c5de0b6ba8a2035bb3ac5fd8206ab6ded3b349b76cd9e4b9f70a9f93aff2fb93491a9ebccc28d3b3419'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BdParallele::Application.config.session_store :cookie_store, key: '_bd_parallele_session'
