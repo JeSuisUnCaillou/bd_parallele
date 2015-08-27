665f30f782f28d35b8a7e0eff34b5587969f673a
jesuisuncaillou <igorlacroute@gmail.com>
2015-08-27 23:57:52 +0200

    fix nested resource tests

--- test/controllers/frames_controller_test.rb
+++ test/controllers/frames_controller_test.rb
@@ -3,47 +3,48 @@ require 'test_helper'
 class FramesControllerTest < ActionController::TestCase
   setup do
     @frame = frames(:one)
+    @ecomic = @frame.ecomic
   end
 
   test "should get index" do
-    get :index
+    get :index, ecomic_id: @ecomic.id
     assert_response :success
     assert_not_nil assigns(:frames)
   end
 
   test "should get new" do
-    get :new
+    get :new, ecomic_id: @ecomic.id
     assert_response :success
   end
 
   test "should create frame" do
     assert_difference('Frame.count') do
-      post :create, frame: { name: @frame.name, ecomic_id: @frame.ecomic  }
+      post :create, frame: { name: @frame.name, ecomic_id: @frame.ecomic }, ecomic_id: @ecomic.id
     end
 
-    assert_redirected_to frame_path(assigns(:frame))
+    assert_redirected_to ecomic_frames_path(@ecomic)
   end
 
   test "should show frame" do
-    get :show, id: @frame
+    get :show, id: @frame, ecomic_id: @ecomic.id
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @frame
+    get :edit, id: @frame, ecomic_id: @ecomic.id
     assert_response :success
   end
 
   test "should update frame" do
-    patch :update, id: @frame, frame: { name: @frame.name }
-    assert_redirected_to frame_path(assigns(:frame))
+    patch :update, id: @frame, frame: { name: @frame.name }, ecomic_id: @ecomic.id
+    assert_redirected_to ecomic_frames_path(@ecomic)
   end
 
   test "should destroy frame" do
     assert_difference('Frame.count', -1) do
-      delete :destroy, id: @frame
+      delete :destroy, id: @frame, ecomic_id: @ecomic.id
     end
 
-    assert_redirected_to frames_path
+    assert_redirected_to ecomic_frames_path(@ecomic)
   end
 end
