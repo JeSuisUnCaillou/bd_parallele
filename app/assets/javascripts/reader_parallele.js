7fc9bd403fd09392a5fe498e6223c64f44d33bad
jesuisuncaillou <igorlacroute@gmail.com>
2015-08-30 21:57:49 +0200

    binding des boutons à l'init

--- app/assets/javascripts/reader_parallele.js
+++ app/assets/javascripts/reader_parallele.js
@@ -16,20 +16,44 @@ function Reader(nb_vertical)
   //Attributes & Init
   this.element=$("#frames_vertical_list");
   this.max_nb_vertical=nb_vertical;
-  this.frame_rows = this.element.children(".frame_row").map( function(i, elem){ return new FrameRow(elem) }).get();
+  this.frame_rows=this.element.children(".frame_row").map(function(i,e){ return new FrameRow(e) }).get();
+  this.frames=$(this.frame_rows).map(function(i,row){ return row.frames }).get();
 
   //Methods
-  this.add_first_frame=function(){
+  this.add_first_framerow=function(){
 
   };
-  this.add_last_frame=function(){
+  this.add_last_framerow=function(){
 
   };
-  this.reorganize=function(){
+  this.organize_frames=function(){
 
   };
 
+  //NEXT EVENT FOR BUTTON
+  this.next_event=function(){
+    button = $(this);
+    console.log("next !");
+  };
+
+  //PREV EVENT FOR BUTTON
+  this.prev_event=function(){
+    button = $(this);
+    console.log("prev !");
+  };
+
+  //Binds both events on buttons;
+  this.assign_buttons=function(elem){
+    elem.find(".next").on("click", this.next_event);
+    elem.find(".prev").on("click", this.prev_event);
+  };
+
+  ////////////////////////////////
+  // Init after defining functions;
+  
+  this.assign_buttons(this.element);
   console.log( "Reader up !");
+  console.log(this);
 };
 
 
@@ -37,14 +61,40 @@ function Reader(nb_vertical)
 // FRAMEROW OBJECT -----------------------------------------------------------------------
 function FrameRow(elem)
 {
-  //Atributes & Init
-  this.element=elem;
-  this.type=this.elem.hasClass(".double_frame") ? "double" : this.elem.hasClass("solo_frame") ? "solo" : "unknown";
-  
+  //Attributes & Init
+  this.element=$(elem);
+  this.frames=this.element.children(".frame").map(function(i,e){ return new Frrame(e) }).get();
+  this.is_double=this.element.hasClass("double_frame");
+  this.is_solo=this.element.hasClass("solo_frame");
+  this.has_cousins=this.element.hasClass("cousins");
+  this.has_siblings=this.element.hasClass("siblings");
+
+  //Methods
 
   console.log("FrameRow up !");
+  console.log(this);
 };
 
+
+
+// FRAMEROW OBJECT -----------------------------------------------------------------------
+function Frrame(elem) // "Frame" est déjà pris :(
+{
+  //Attributes & Init
+  this.element=$(elem);
+  this.id=this.element.attr("data-id");
+  this.parent_id=this.element.attr("data-parent_id");
+  this.is_left=this.element.hasClass("left_frame");
+  this.is_right=this.element.hasClass("right_frame");
+  this.is_central=this.element.hasClass("central_frame");
+  this.next_button=this.element.find(".next");
+  this.prev_button=this.element.find(".prev");
+
+  //Methods
+
+  console.log("Frame up !");
+  console.log(this);
+}
 
 
 
